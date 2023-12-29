879e12f619e91301c2996e1f91c05ae3de5f523d
ravik <ravik@192.168.10.37>
2023-12-29 20:46:50 +0530

    second

---
+++ AllureReporting/src/test/java/testing.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ravik
+ *
+ */
+public class testing {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
